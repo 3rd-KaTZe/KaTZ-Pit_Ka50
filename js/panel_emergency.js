d53f592c52ccd4589a4c0b3716be8bdb0fa6ccb9
3rd-KaTZe <le_chat@perrin-lucas.com>
2015-02-18 17:50:54 +0100

    Implementation DCS Focus

--- js/panel_emergency.js
+++ js/panel_emergency.js
@@ -4,10 +4,11 @@ function panel_emergency_update(KaTZPit_data){
 	// Voyent Master warning
 	if (KaTZPit_data["Master_Warn"] ==1) {$("#VE_Alarm").fadeIn()} else {$("#VE_Alarm").fadeOut()}
 	
-	
 	// Debug
 	// Affichage des Valeurs tests affichées sous le bouton marche
 	var total_error = total_error + KaTZPit_data["Error"]
+	
+	
 	document.getElementById('IP_Host').innerHTML = serverws.ip
 	document.getElementById('IP_Port').innerHTML = serverws.port
 	
